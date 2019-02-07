95c602b80968a79cce107c17f4ddf670c8c705a8
Goh Sato <gsato@uvic.ca>
2019-02-06 19:47:45 -0800

    Did a bit of research

--- highlighter.py
+++ highlighter.py
@@ -7,14 +7,21 @@ from matplotlib import pyplot as plt
 
 print(cv2.__file__)
 
-img = cv2.imread('images/image_1.jpg',1)[:,:,::-1]
+img = cv2.imread('images/image_1.jpg',0)
 
-img = img[:,:,2]
-
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+print(img.shape)
+# img = cv2.GaussianBlur(img,(5,5),0)
 
 
+block_size = 35
+
+substraction_constant = 2
+
+th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,block_size,substraction_constant)
+
+
+img = th3
 plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 plt.show()
---
+++ notes_opencv.md
@@ -0,0 +1,9 @@
+# OpenCV
+Some notes on opencv
+
+## Core
+top left is  (0,0)
+bottom right is (len(x),len(y))
+
+images are stored as (x,y,c)  
+where c = (B,G,R) values
