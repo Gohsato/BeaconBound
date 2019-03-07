5bf0177978314b834a71715fb8c120949b0a1f52
Goh Sato <gsato@uvic.ca>
2019-03-06 19:13:07 -0800

    Initial basic strategies
    brightest pixel and adaptive thresholding

--- highlighter.py
+++ highlighter.py
@@ -5,23 +5,37 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-print(cv2.__file__)
+while(True):
+    print(cv2.__file__)
 
-img = cv2.imread('images/image_1.jpg',0)
+    img = cv2.imread('images/image_faulty.jpg',0)
 
-print(img.shape)
-# img = cv2.GaussianBlur(img,(5,5),0)
+    print(img.shape)
 
+    orig = img
 
-block_size = 35
+    # blurring
+    blur_radius = 51
+    blurred_img = cv2.GaussianBlur(img,(blur_radius,blur_radius),0)
 
-substraction_constant = 2
+    plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+    plt.show()
 
-th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY,block_size,substraction_constant)
+    # Find Brghtest Pixel and circle
+    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(blurred_img)
+    image = orig.copy()
+    circle_radius = 25
+    cv2.circle(image, maxLoc, circle_radius, (255, 0, 0), 2)
 
+    plt.imshow(image, cmap = 'gray', interpolation = 'bicubic')
+    plt.show()
 
-img = th3
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.show()
+    # Thresolding
+    block_size = 5
+    substraction_constant = 0
+
+    thres_image = cv2.adaptiveThreshold(blurred_img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+                cv2.THRESH_BINARY,block_size,substraction_constant)
+
+    plt.imshow(thres_image, cmap = 'gray', interpolation = 'bicubic')
+    plt.show()
